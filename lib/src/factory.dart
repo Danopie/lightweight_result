86ee67a1b87e7ea2f920f98bc6511b1ef2f277c4
DanLe <danle.sdev@gmail.com>
2020-06-18 18:46:18 +0700

    Add test and document for ResultFactory

--- lib/src/factory.dart
+++ lib/src/factory.dart
@@ -1,9 +1,21 @@
 import 'package:result_monad/src/result.dart';
 
-Result<T, Exception> runCatching<T>(T Function() func) {
+/// Calls the specified function [block] and returns its encapsulated result if invocation was successful, catching and encapsulating any thrown exception as a failure.
+Result<T, Exception> runCatching<T>(T Function() block) {
   try {
-    return Result.ok(func());
+    return Result.ok(block());
   } on Exception catch (e) {
     return Result.failure(e);
+  }
+}
+
+/// Converts a nullable of type [T] to a [Result]. Returns a success if the value is non-null, otherwise the supplied [error].
+extension ResultFactory<T, E> on T {
+  Result<T, E> toResultOr(E Function() error) {
+    if (this != null) {
+      return Result.ok(this);
+    } else {
+      return Result.failure(error());
+    }
   }
 }
---
+++ test/factory_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:result_monad/result_monad.dart';
+
+class CallbackException implements Exception {}
+
+void main() {
+  group("test runCatching", () {
+    test("runCatching() should return value upon success", () {
+      final callback = (){
+        return "example";
+      };
+
+      expect(runCatching(callback).get(), "example");
+    });
+
+    test("runCatching() should return exception upon success", () {
+      final callback = (){
+        throw CallbackException();
+      };
+
+      expect(runCatching(callback).get(), isNull);
+      expect(runCatching(callback).getError(), isA<CallbackException>());
+    });
+  });
+
+  group("test toResultOr", () {
+    test("toResultOr() should return success if not null", () {
+      expect("ok".toResultOr(() => "wait").get(), "ok");
+    });
+
+    test("toResultOr() should return error if null", () {
+      expect(null.toResultOr(() => "wait").getError(), "wait");
+      expect(null.toResultOr(() => "wait").get(), isNull);
+    });
+  });
+
+}
