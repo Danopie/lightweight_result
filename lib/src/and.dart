2f3939a2f0a01fd4329d8a19a6f887b41ad00f35
DanLe <danle.sdev@gmail.com>
2020-06-18 17:38:49 +0700

    Add test and document for ResultAnd

--- lib/src/and.dart
+++ lib/src/and.dart
@@ -1,6 +1,7 @@
 import 'package:result_monad/src/result.dart';
 
 extension ResultAnd<T, E> on Result<T, E> {
+  /// Returns [value] if this [Result] is a success, otherwise this [error].
   Result<T, E> and(Result<T, E> Function() result) {
     if (isSuccess)
       return result();
@@ -8,6 +9,8 @@ extension ResultAnd<T, E> on Result<T, E> {
       return this;
   }
 
+  /// Maps this [Result<T, E>][Result] to [Result<U, E>][Result] by either applying the [transform]
+  /// function if this [Result] is a success, or returning this [error].
   Result<U, E> andThen<U>(Result<U, E> Function(T) transform) {
     if (isSuccess)
       return transform(value);
---
+++ test/and_test.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:result_monad/result_monad.dart';
+
+class AndError implements Exception {}
+
+void main() {
+  group("test and", () {
+    test("and() should return value upon success", () {
+      expect(Result.ok(230).and(() => Result.ok(500)).get(), 500);
+    });
+
+    test("and() should return new error upon success", () {
+      expect(Result.ok(300).and(() => Result.failure(AndError())).getError(),
+          isA<AndError>());
+    });
+  });
+
+  group("test andThen", () {
+    test("andThen() should return transformed value upon success", () {
+      expect(Result.ok(5).andThen((value) => Result.ok(value + 7)).get(), 12);
+    });
+
+    test("andThen() should return new error upon failure", () {
+      expect(
+          Result.ok(20)
+              .andThen((value) => Result.ok(value + 43))
+              .andThen((value) => Result.failure(AndError()))
+              .getError(),
+          isA<AndError>());
+    });
+  });
+}
