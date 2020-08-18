4db6a2ad5ac004c00e7166a16bf8bcef1a99fbd1
DanLe <danle.sdev@gmail.com>
2020-08-18 18:34:14 +0700

    Add runCatchingAsync

--- lib/src/factory.dart
+++ lib/src/factory.dart
@@ -9,6 +9,16 @@ Result<T, Exception> runCatching<T>(T Function() block) {
   }
 }
 
+/// Calls and await the specified function [block] and returns its encapsulated result if invocation was successful, catching and encapsulating any thrown exception as a failure.
+Future<Result<T, Exception>> runCatchingAsync<T>(
+    Future<T> Function() block) async {
+  try {
+    return Result.ok(await block());
+  } on Exception catch (e) {
+    return Result.err(e);
+  }
+}
+
 /// Converts a nullable of type [T] to a [Result]. Returns a success if the value is non-null, otherwise the supplied [error].
 extension ResultFactory<T, E> on T {
   Result<T, E> toResultOr(E Function() error) {
